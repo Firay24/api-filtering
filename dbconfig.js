30def68fd1b2401ed94c50df04eacff6a82480dc
Hatakee Fira <fisan.onodaconan@gmail.com>
2023-09-29 20:01:12 +0700

    config database

--- dbconfig.js
+++ dbconfig.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2')
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Midorima240",
-    database: "order"
+    host: "sql12.freesqldatabase.com",
+    user: "sql12649938",
+    password: "PlIT5EDfpQ",
+    database: "sql12649938"
 })
 
 db.connect(function(err){
